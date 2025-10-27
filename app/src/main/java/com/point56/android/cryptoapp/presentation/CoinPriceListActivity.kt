d47f42d4cedee1e6b5e565d7af97de0e96e802ed
sla <slaler@outlook.com>
2025-10-26 22:31:07 -0400

    Landscape mode with Fragment in Activity

--- app/src/main/java/com/point56/android/cryptoapp/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/point56/android/cryptoapp/presentation/CoinPriceListActivity.kt
@@ -3,6 +3,7 @@ package com.point56.android.cryptoapp.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.point56.android.cryptoapp.R
 import com.point56.android.cryptoapp.databinding.ActivityCoinPriceListBinding
 import com.point56.android.cryptoapp.domain.CoinInfo
 import com.point56.android.cryptoapp.presentation.adapters.CoinInfoAdapter
@@ -24,11 +25,11 @@ class CoinPriceListActivity : AppCompatActivity() {
         val adapter = CoinInfoAdapter(this)
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinInfo) {
-                val intent = CoinDetailActivity.newIntent(
-                    this@CoinPriceListActivity,
-                    coinPriceInfo.fromSymbol
-                )
-                startActivity(intent)
+                if (isOnePaneMode()) {
+                    launchDetailActivity(fromSymbol = coinPriceInfo.fromSymbol)
+                } else {
+                    launchDetailFragment(coinPriceInfo.fromSymbol)
+                }
             }
         }
         binding.rvCoinPriceList.adapter = adapter
@@ -37,4 +38,24 @@ class CoinPriceListActivity : AppCompatActivity() {
             adapter.submitList(it)
         }
     }
+
+    private fun isOnePaneMode() = binding.fragmentContainer == null
+
+    private fun launchDetailActivity(fromSymbol: String) {
+        val intent = CoinDetailActivity.newIntent(
+            this@CoinPriceListActivity,
+            fromSymbol
+        )
+        startActivity(intent)
+    }
+
+    private fun launchDetailFragment(fromSymbol: String) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+            .addToBackStack(null)
+            .commit()
+    }
+
 }
