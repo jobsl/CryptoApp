cc753ca1ad471b6e898af3618dcafca06df02f66
sla <slaler@outlook.com>
2025-11-15 20:30:15 -0500

    add dependencies into RefreshDataWorker

---
+++ app/src/main/java/com/point56/android/cryptoapp/data/workers/RefreshDataWorkerFactory.kt
@@ -0,0 +1,31 @@
+package com.point56.android.cryptoapp.data.workers
+
+import android.content.Context
+import androidx.work.ListenableWorker
+import androidx.work.WorkerFactory
+import androidx.work.WorkerParameters
+import com.point56.android.cryptoapp.data.database.CoinInfoDao
+import com.point56.android.cryptoapp.data.mapper.CoinMapper
+import com.point56.android.cryptoapp.data.network.ApiService
+
+class RefreshDataWorkerFactory(
+    private val coinInfoDao: CoinInfoDao,
+    private val apiService: ApiService,
+    private val mapper: CoinMapper
+): WorkerFactory() {
+    override fun createWorker(
+        appContext: Context,
+        workerClassName: String,
+        workerParameters: WorkerParameters
+    ): ListenableWorker? {
+
+        return RefreshDataWorker(
+            appContext,
+            workerParameters,
+            coinInfoDao,
+            apiService,
+            mapper
+        )
+
+    }
+}
--- app/src/main/java/com/point56/android/cryptoapp/presentation/CoinApp.kt
+++ app/src/main/java/com/point56/android/cryptoapp/presentation/CoinApp.kt
@@ -1,12 +1,27 @@
 package com.point56.android.cryptoapp.presentation
 
 import android.app.Application
+import androidx.work.Configuration
+import com.point56.android.cryptoapp.data.database.AppDatabase
+import com.point56.android.cryptoapp.data.mapper.CoinMapper
+import com.point56.android.cryptoapp.data.network.ApiFactory
+import com.point56.android.cryptoapp.data.workers.RefreshDataWorkerFactory
 import com.point56.android.cryptoapp.di.DaggerApplicationComponent
 
-class CoinApp : Application() {
+class CoinApp : Application(), Configuration.Provider {
 
     val component by lazy {
         DaggerApplicationComponent.factory().create(this)
     }
 
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(
+                RefreshDataWorkerFactory(
+                    AppDatabase.getInstance(this).coinPriceInfoDao(),
+                    ApiFactory.apiService,
+                    CoinMapper()
+                )
+            )
+            .build()
 }
